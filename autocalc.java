1a03b49982417a8107c8c3e34d272cc7ac9cf39a
IBush <100794571+0IBush0@users.noreply.github.com>
2022-06-10 08:12:40 -0600

    Create autocalc.java

---
+++ autocalc.java
@@ -0,0 +1,23 @@
+import com.phidget22.*;
+
+public class autocalc 
+{
+    static VoltageRatioInput scale;
+    
+    public static double getAverage()throws Exception
+    {
+        double average = 0;
+        int count = 0;
+        System.out.println("Averaging Value...");
+        while(count < 64)
+        {
+            average += scale.getVoltageRatio();
+            count += 1;
+            Thread.sleep(20);
+            //time
+        }
+        return average/count;
+        
+     }
+     
+}
