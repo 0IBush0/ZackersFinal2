3efc34afbc13922749f8105dcfa19f319f9b40b7
IBush <100794571+0IBush0@users.noreply.github.com>
2022-06-10 08:09:22 -0600

    Create scalelb.java

---
+++ scalelb.java
@@ -0,0 +1,27 @@
+import com.phidget22.*;
+
+public class PhidgetScale2_lb {
+    public static void main(String[] args) throws Exception{
+
+        //Create
+        VoltageRatioInput scale = new VoltageRatioInput();
+        
+        //Open
+        scale.open(1000);
+        
+        //Use your Offset Value
+        double offsetValue = 3.530131654595086E-5;
+        
+        //Use your Phidgets
+        while(true)
+        {
+            //Calculate Weight (kg)
+            double weight = (4700 * (scale.getVoltageRatio() - offsetValue)) * 2.205;
+            
+            System.out.println(String.format("%.3f lb", weight));      
+            //display weight in lbs
+            Thread.sleep(250);
+          
+        }
+    }    
+}
